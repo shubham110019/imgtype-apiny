06355cdc2bc4391d177464ac8476710c40c74b6c
Shubham Ralli <shubhamralli@Shubhams-MacBook-Pro.local>
2023-08-15 22:02:57 +0530

    some change

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const sharp = require('sharp');
 const multer = require('multer');
 const cors = require('cors');
+const fs = require('fs');
+const path = require('path');
+const os = require('os');
 
 const app = express();
 const port = 3000;
@@ -95,17 +98,34 @@ app.post('/convert', checkAuthKey, upload.array('image', 50), async (req, res) =
         imageWidth = `${svgMetadata.width}px`;
       }
 
+      const originalFilename = file.originalname;
+      const imageName = originalFilename.split('.').slice(0, -1).join('.');
+
+       // Create a temporary file to access its path
+  const tempFilePath = path.join(os.tmpdir(), file.originalname);
+  fs.writeFileSync(tempFilePath, file.buffer);
+
+  // Get the last modified date of the temporary file
+  const fileStats = fs.statSync(tempFilePath);
+  const lastModifiedDate = fileStats.mtime;
+
+  // Clean up the temporary file
+  fs.unlinkSync(tempFilePath);
+
       const conversionResult = {
         originalSize: formatBytes(originalSize),
         newImageSize: formatBytes(newImageSize),
         renderedSize: renderedImageSize,
         imageHeight,
+        imageName,
         imageWidth,
         conversionTime,
         downloadLink,
         format,
         imageSrc,
-        oldFormat: file.originalname.split('.').pop().toLowerCase()
+        fileName: file.originalname, 
+        oldFormat: file.originalname.split('.').pop().toLowerCase(),
+        lastModifiedDate,
       };
 
       conversionResults.push(conversionResult);
@@ -120,6 +140,85 @@ app.post('/convert', checkAuthKey, upload.array('image', 50), async (req, res) =
     res.status(500).json({ error: 'An error occurred during conversion' });
   }
 });
+
+
+
+app.post('/compress', checkAuthKey, upload.array('image', 50), async (req, res) => {
+  try {
+    const compressionResults = [];
+
+    for (const file of req.files) {
+      const inputBuffer = file.buffer;
+      const originalSize = inputBuffer.length;
+
+      const quality = parseInt(req.body.quality) || 80; // Quality parameter (default: 80)
+
+      const startTime = Date.now();
+
+      const format = file.originalname.split('.').pop().toLowerCase(); // Get the desired format from the request body, default to jpeg
+
+      const compressedImageBuffer = await sharp(inputBuffer)
+        .toFormat(format)
+        .jpeg({ quality }) // Always convert to JPEG format before compressing
+        .toBuffer();
+
+      const endTime = Date.now();
+      const compressionTime = endTime - startTime;
+
+      const newImageSize = compressedImageBuffer.length;
+
+      const renderedImage = await sharp(compressedImageBuffer)
+        .toBuffer();
+      const renderedImageMetadata = await sharp(renderedImage).metadata();
+      const renderedSize = `${renderedImageMetadata.width}x${renderedImageMetadata.height}`;
+      const imageHeight = `${renderedImageMetadata.height}px`;
+      const imageWidth = `${renderedImageMetadata.width}px`;
+
+      const compressionPercentage = ((originalSize - newImageSize) / originalSize) * 100;
+
+      const originalFilename = file.originalname;
+      const imageName = originalFilename.split('.').slice(0, -1).join('.');
+
+     // Create a temporary file to access its path
+  const tempFilePath = path.join(os.tmpdir(), file.originalname);
+  fs.writeFileSync(tempFilePath, file.buffer);
+
+  // Get the last modified date of the temporary file
+  const fileStats = fs.statSync(tempFilePath);
+  const lastModifiedDate = fileStats.mtime;
+
+  // Clean up the temporary file
+  fs.unlinkSync(tempFilePath);
+
+      const compressionResult = {
+        originalSize: formatBytes(originalSize),
+        newImageSize: formatBytes(newImageSize),
+        quality,
+        compressionTime,
+        renderedSize,
+        imageHeight,
+        imageWidth,
+        fileName: file.originalname,
+        imageName,
+        downloadLink: `data:image/${format};base64,${compressedImageBuffer.toString('base64')}`,
+        format, // Store the format for reference
+        oldFormat: file.originalname.split('.').pop().toLowerCase(),
+        compressionPercentage: compressionPercentage.toFixed(2) + '%',
+        lastModifiedDate
+      };
+
+      compressionResults.push(compressionResult);
+    }
+
+    const responseObject = { compress: compressionResults };
+    res.json(responseObject);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred during compression' });
+  }
+});
+
+
 
 app.listen(process.env.PORT || port, () => {
   console.log(`Server is running on port ${port}`);
